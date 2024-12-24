9aecadad6f48f5711108645b11a84702bc6dee6f
LojainHamza <lojainehab17@gmail.com>
2024-12-24 02:44:03 +0200

    finished love tab ui.

---
+++ lib/ui/home_screen/tabs/custom_text_field.dart
@@ -0,0 +1,59 @@
+import 'package:event_planning_app/utils/MyAppStyles.dart';
+import 'package:event_planning_app/utils/myAppColors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  Color? borderColor;
+  String? hintText;
+  String? labelText;
+  TextStyle? hintStyle;
+  TextStyle? labelStyle;
+  TextStyle? style;
+  Widget? prefixIcon;
+  Widget? suffixIcon;
+  CustomTextField({this.borderColor,required this.hintText, this.labelText, this.hintStyle, this.labelStyle, this.style, this.prefixIcon, this.suffixIcon});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: MyAppColors.blackColor,
+      style: style ?? MyAppStyles.medium16Black,
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelText: labelText,
+        hintStyle: hintStyle ?? MyAppStyles.medium16Gray,
+        labelStyle: labelStyle ?? MyAppStyles.medium16Gray,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+            color: borderColor ?? MyAppColors.grayColor,
+            width: 2
+          )
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(
+                color: borderColor ?? MyAppColors.grayColor,
+                width: 2
+            )
+        ),
+        errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(
+                color: MyAppColors.redColor,
+                width: 2
+            )
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(
+                color: MyAppColors.redColor,
+                width: 2
+            )
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/home_screen/tabs/love/love_tab.dart
+++ lib/ui/home_screen/tabs/love/love_tab.dart
@@ -1,13 +1,52 @@
+import 'package:event_planning_app/ui/home_screen/tabs/custom_text_field.dart';
+import 'package:event_planning_app/ui/home_screen/tabs/home/event_item_widget.dart';
 import 'package:event_planning_app/utils/MyAppColors.dart';
+import 'package:event_planning_app/utils/MyAppStyles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoveTab extends StatelessWidget {
   const LoveTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      color: MyAppColors.redColor,
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: height*0.05
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: width*0.02,
+              ),
+              child: CustomTextField(
+                style: MyAppStyles.medium16Primary,
+                borderColor: MyAppColors.primaryLight,
+                hintText: AppLocalizations.of(context)!.search_for_event,
+                hintStyle: MyAppStyles.bold14Primary,
+                prefixIcon: const Icon(Icons.search,color: MyAppColors.primaryLight),
+              ),
+            ),
+            Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: width*0.04,
+                  ),
+                  child: ListView.builder(
+                      itemCount: 20,
+                      itemBuilder: (context,index){
+                        return EventItemWidget();
+                      }
+                  ),
+                )
+            )
+          ],
+        ),
+      ),
     );
   }
 }
