f4461e3310e1fa877cabf1b6944ea7e8a63f0d72
LojainHamza <lojainehab17@gmail.com>
2025-01-10 14:47:00 +0200

    incomplete user provider.

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,8 @@
+import 'package:event_planning_app/model/myUser.dart';
+import 'package:flutter/material.dart';
+
+class UserProvider extends ChangeNotifier{
+  MyUser? currentUser;
+
+  static updateUser(){}
+}
